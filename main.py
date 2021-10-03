ed5fe1645518e74137e234d26925693a6d547a1b
sanjin1110 <thapasanjin00@gmail.com>
2021-10-03 11:26:31 +0545

    few changes

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ def check():  # Checks for victory or Draw
             messagebox.showinfo("Congrats!!", "'" + name_entry1.get() + " ' has won")
             score1 += 1
             with open("scoreboard.txt ", "a") as file_object:
-                file_object.writelines(name_entry1.get()+" "+"score=" + str(score1) + "\n")
+                file_object.writelines(name_entry1.get()+" "+"score =" + str(score1) + "\t")
 
             reset()
     if b[0][0]["text"] == b[1][1]["text"] == b[2][2]["text"] == a or b[0][2]["text"] == b[1][1]["text"] \
@@ -88,7 +88,7 @@ def check():  # Checks for victory or Draw
         messagebox.showinfo("Congrats!!", "'" + name_entry2.get() + "' has won")
         score2 += 1
         with open("scoreboard.txt ", "a") as file_object:
-            file_object.writelines(name_entry2.get()+" "+"score=" + str(score1) + "\n")
+            file_object.writelines(name_entry2.get()+" "+"score =" + str(score2) + "\n")
 
         reset()
     elif b[0][0]["state"] == b[0][1]["state"] == b[0][2]["state"] == b[1][0]["state"] == b[1][1]["state"] \
